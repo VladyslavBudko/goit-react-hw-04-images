ee1e51807f56538344a66faf9108dfe0d6785adf
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-11-07 01:43:19 +0200

    add index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,8 @@
-export const App = () => {
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+const App = () => {
   return (
     <div
       style={{
@@ -7,10 +11,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+export default App;
--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,3 +1,5 @@
+// import styled from 'styled-components';
+
 .App {
     display: grid;
     grid-template-columns: 1fr;
