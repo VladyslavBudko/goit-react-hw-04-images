561529ee0ca7874904a83a7d3749d9915645aab7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-11-07 03:14:10 +0200

    button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import {LoadMoreButton} from './Button.styled'
+
+const LoadMoreBtn = () => {
+  return <LoadMoreButton type="button">Load more</LoadMoreButton>;
+};
+
+export default LoadMoreBtn;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,14 @@
 import { ImageGalleryList } from './ImageGallery.styled';
 import ImageGalleryItem from 'components/ImageGalleryItem'
+import LoadMoreBtn from 'components/Button'
+
 const ImageGallery = () => {
   return (
     <ImageGalleryList>
       {/* <!-- Набор <li> с изображениями --> */}
 
       <ImageGalleryItem/>
+      <LoadMoreBtn/>
     </ImageGalleryList>
   );
 };
